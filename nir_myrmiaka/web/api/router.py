cd15eaead9eced07dfd54a85678fbb531cc4cd08
abyssal <abyssal@example.xom>
2024-11-02 09:38:52 -0200

    next student

--- nir_myrmiaka/web/api/router.py
+++ nir_myrmiaka/web/api/router.py
@@ -1,6 +1,6 @@
 from fastapi.routing import APIRouter
 
-from nir_myrmiaka.web.api.v1 import monitoring, auth, user
+from nir_myrmiaka.web.api.v1 import monitoring, auth, user, student
 
 from nir_myrmiaka.settings import settings
 
@@ -9,3 +9,4 @@ api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(auth.router, prefix=f"/{settings.api_version}/auth")
 api_router.include_router(user.router, prefix=f"/{settings.api_version}/user")
+api_router.include_router(student.router, prefix=f"/{settings.api_version}/student")
---
+++ nir_myrmiaka/web/api/v1/student/__init__.py
@@ -0,0 +1,3 @@
+from nir_myrmiaka.web.api.v1.student.views import router
+
+__all__ = ["router"]
